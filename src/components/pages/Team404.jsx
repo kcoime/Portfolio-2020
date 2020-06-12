33d1df5aeddf7d6614d9bad43b00116253e4228e
kcoime <60752219+kcoime@users.noreply.github.com>
2020-06-12 11:07:19 -0700

    added proper react syntax to 404

--- src/components/pages/Team404.jsx
+++ src/components/pages/Team404.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Team404 = () => {
     return(
         <div className="wrapper">
-            <h1><span>{'<'}</span>404 Not Found<span>/></span></h1>
+            <h1><span>{'<'}</span>404 Not Found<span>{'/>'}</span></h1>
         </div>
     )
 };
