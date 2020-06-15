b33e3a75ad85fa315505c267e8b11e0f79f25317
kcoime <60752219+kcoime@users.noreply.github.com>
2020-06-14 19:46:45 -0700

    added cinewatch folder to public

---
+++ public/cinewatch/precache-manifest.4a793c914e43872013b655a2722f809c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0c97847c967b84c84602cc8bc7b70e",
+    "url": "/cinewatch/index.html"
+  },
+  {
+    "revision": "cc86ccd358585c73d688",
+    "url": "/cinewatch/static/css/main.2b30ddf8.chunk.css"
+  },
+  {
+    "revision": "16bc9cfb378ab4ebd565",
+    "url": "/cinewatch/static/js/2.0de668ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cinewatch/static/js/2.0de668ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc86ccd358585c73d688",
+    "url": "/cinewatch/static/js/main.16b1379d.chunk.js"
+  },
+  {
+    "revision": "e6fbcc200c6185a42626",
+    "url": "/cinewatch/static/js/runtime-main.18eaf204.js"
+  }
+]);
